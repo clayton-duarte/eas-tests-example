08dddf414194074d9bdd22cb2720b010084a36c9
cpd <cpd@duck.com>
2023-06-26 10:24:51 -0400

    :sparkles: Implements simple counter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setCount(count - 1)}
+        >
+          <Text>Decrement</Text>
+        </TouchableOpacity>
+        <Text>{count}</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setCount(count + 1)}
+        >
+          <Text>Increment</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+  },
+  button: {
+    backgroundColor: "#ccc",
+    padding: 8,
   },
 });
